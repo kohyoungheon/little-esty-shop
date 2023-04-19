d460a236bab9b9bbd5689a97540033e19b9e82fd
Caroline Peri <caroline.anne.peri@gmail.com>
2023-04-19 18:58:32 -0400

    fix: add migration to add status to merchants

--- db/migrate/20230410224299_create_merchants.rb
+++ db/migrate/20230410224299_create_merchants.rb
@@ -3,7 +3,7 @@ class CreateMerchants < ActiveRecord::Migration[5.2]
     create_table :merchants do |t|
 
       t.string :name, null: false
-      t.boolean :is_enabled, default: false
+
       t.timestamp :created_at, null: false
       t.timestamp :updated_at, null: false
     end
